d8c07ca7b0fa3292e36a490af623f30fe8534969
VerkhoturovIP <verkhoturov314@gmail.com>
2025-06-19 19:20:36 +0300

    debug

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,4 +1,20 @@
-import PageTemplate, { generateMetadata } 
-from './[slug]/page';
-export default PageTemplate;
-export { generateMetadata };
+export const revalidate = 0;
+
+import { notFound } from 'next/navigation';
+import { Home } from '@/page-templates/home';
+import { getPayload } from 'payload';
+import configPromise from '@payload-config';
+
+export default async function Page() {
+  const payload = await getPayload({ config: configPromise });
+
+  const page = await payload.findGlobal({
+    slug: 'home-page',
+  });
+
+  if (!page) {
+    return notFound();
+  }
+
+  return <Home {...page} />;
+}
